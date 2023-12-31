38bbb4b30d798c479c0e670588a84af01ec91964
tahirshaikh85 <tahirshaikhrs7@gmail.com>
2023-12-31 16:23:46 +0530

    create slider form

--- src/components/homepage/Slider/Slider.jsx
+++ src/components/homepage/Slider/Slider.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Slider = () => {
     const [apiData, setApiData] = useState([]);
+    const navigate = useNavigate()
 
     useEffect(() => {
         fetch('http://localhost:8080/api/getImgCircle')
@@ -20,40 +22,47 @@ const Slider = () => {
     };
     return (
         <>
-        <div className='flex justify-between mt-6 mx-4'>
-            {apiData && apiData.map((item) => (
-                <div key={item._id} style={{ marginBottom: '20px', position: "relative" }}>
-                    <img src={item.imgSrc} alt="Product" style={{ maxWidth: '100%', width: "400px", height: "200px" }} />
-                    {item.circles.map((circle) => (
-                        <div
-                            key={circle._id}
-                            style={{
-                                position: 'absolute',
-                                top: `${circle.topPosition}%`,
-                                left: `${circle.leftPosition}%`,
-                                transform: 'translate(-50%, -50%)',
-                                background: 'red',
-                                color: 'white',
-                                padding: '5px',
-                                borderRadius: '50%',
-                            }}
-                        >
-                            <p>{circle.productTitle}</p>
-                            <p>{circle.productCategory}</p>
-                            <p>${circle.price.toFixed(2)}</p>
-                            <button
-                                type="button"
-                                className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-1"
-                                onClick={() => handleDelete(item._id)}
-                            >
-                                Delete
-                            </button>
+            <div className='mt-6 mx-4'>
+                <div className='text-center mb-6'>
+                    <h1 className='font-bold '>Slider section</h1>
+                    <span className='text-purple-400 font-bold  mt-1 hover:cursor-pointer' onClick={() => navigate('/homePage/create-slider-section')}>Create New</span>
+                </div>
+                <div className='flex justify-between'>
+
+                    {apiData && apiData.map((item) => (
+                        <div key={item._id} style={{ marginBottom: '20px', position: "relative" }}>
+                            <img src={item.imgSrc} alt="Product" style={{ maxWidth: '100%', width: "400px", height: "200px" }} />
+                            {item.circles.map((circle) => (
+                                <div
+                                    key={circle._id}
+                                    style={{
+                                        position: 'absolute',
+                                        top: `${circle.topPosition}%`,
+                                        left: `${circle.leftPosition}%`,
+                                        transform: 'translate(-50%, -50%)',
+                                        background: 'red',
+                                        color: 'white',
+                                        padding: '5px',
+                                        borderRadius: '50%',
+                                    }}
+                                >
+                                    <p>{circle.productTitle}</p>
+                                    <p>{circle.productCategory}</p>
+                                    <p>${circle.price?.toFixed(2)}</p>
+                                    <button
+                                        type="button"
+                                        className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-1"
+                                        onClick={() => handleDelete(item._id)}
+                                    >
+                                        Delete
+                                    </button>
+                                </div>
+                            ))}
                         </div>
                     ))}
                 </div>
-            ))}
-        </div>
-    </>
+            </div>
+        </>
     )
 }
 
---
+++ src/components/homepage/Slider/SliderForm.jsx
@@ -0,0 +1,166 @@
+import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import AdminNavbar from '../../AdminNavbar';
+import { useNavigate } from 'react-router-dom';
+
+function SliderForm() {
+    const { handleSubmit, control } = useForm();
+    const navigate = useNavigate();
+
+    const onSubmit = async (data) => {
+        try {
+            console.log(data);
+            const response = await fetch('http://localhost:8080/api/createImgCricle', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+
+            const responseData = await response.json();
+            window.alert(responseData.message);
+            navigate('/homePage')
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    return (
+        <>
+            <AdminNavbar />
+            <form onSubmit={handleSubmit(onSubmit)} className="max-w-md mx-auto p-6 border rounded-md shadow-md mt-10">
+                <div className="mt-6">
+
+                    <label htmlFor={`imgSrc`} className="block text-sm font-medium leading-5 text-gray-700 mt-4">
+                        Image Source
+                    </label>
+                    <Controller
+                        name={`imgSrc`}
+                        control={control}
+                        defaultValue={[]}
+                        render={({ field }) => (
+                            <input
+                                {...field}
+                                type="text"
+                                className="mt-1 p-2 border block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 rounded-md"
+                            />
+                        )}
+                    />
+
+                    <label className="block text-sm font-medium leading-5 text-gray-700 mt-4">Circles</label>
+                    <Controller
+                        name={`circles`}
+                        control={control}
+                        defaultValue={[]}
+                        render={({ field }) => (
+                            <div>
+                                {field.value.map((circle, index) => (
+
+                                    <div key={index} className="mt-4">
+
+                                        <label htmlFor={`circles[${index}].productTitle`} className="block text-sm font-medium leading-5 text-gray-700">
+                                            Circle {index + 1} Product Title
+                                        </label>
+                                        <Controller
+                                            name={`circles[${index}].productTitle`}
+                                            control={control}
+                                            defaultValue=""
+                                            render={({ field }) => (
+                                                <input
+                                                    {...field}
+                                                    type="text"
+                                                    className="mt-1 p-2 border block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 rounded-md"
+                                                />
+                                            )}
+                                        />
+
+
+                                        <label htmlFor={`circles[${index}].productCategory`} className="block text-sm font-medium leading-5 text-gray-700 mt-4">
+                                            Circle {index + 1} Product Category
+                                        </label>
+                                        <Controller
+                                            name={`circles[${index}].productCategory`}
+                                            control={control}
+                                            defaultValue=""
+                                            render={({ field }) => (
+                                                <input
+                                                    {...field}
+                                                    type="text"
+                                                    className="mt-1 p-2 border block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 rounded-md"
+                                                />
+                                            )}
+                                        />
+
+
+                                        <label htmlFor={`circles[${index}].productCategory`} className="block text-sm font-medium leading-5 text-gray-700 mt-4">
+                                            Circle {index + 1} Product Price
+                                        </label>
+                                        <Controller
+                                            name={`circles[${index}].productPrice`}
+                                            control={control}
+                                            defaultValue=""
+                                            render={({ field }) => (
+                                                <input
+                                                    {...field}
+                                                    type="number"
+                                                    className="mt-1 p-2 border block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 rounded-md"
+                                                />
+                                            )}
+                                        />
+
+                                        <label htmlFor={`circles[${index}].topPosition`} className="block text-sm font-medium leading-5 text-gray-700 mt-4">
+                                            Circle {index + 1} Top Position
+                                        </label>
+                                        <Controller
+                                            name={`circles[${index}].topPosition`}
+                                            control={control}
+                                            defaultValue=""
+                                            render={({ field }) => (
+                                                <input
+                                                    {...field}
+                                                    type="number"
+                                                    className="mt-1 p-2 border block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 rounded-md"
+                                                />
+                                            )}
+                                        />
+
+                                        <label htmlFor={`circles[${index}].leftPosition`} className="block text-sm font-medium leading-5 text-gray-700 mt-4">
+                                            Circle {index + 1} Left Position
+                                        </label>
+                                        <Controller
+                                            name={`circles[${index}].leftPosition`}
+                                            control={control}
+                                            defaultValue=""
+                                            render={({ field }) => (
+                                                <input
+                                                    {...field}
+                                                    type="number"
+                                                    className="mt-1 p-2 border block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 rounded-md"
+                                                />
+                                            )}
+                                        />
+                                    </div>
+                                ))}
+                                <button type="button" onClick={() => field.onChange([...field.value, {}])} className="text-indigo-600 hover:text-indigo-900 mt-4">
+                                    Add Circle
+                                </button>
+                            </div>
+                        )}
+                    />
+                </div>
+
+                <div className="mt-8">
+                    <button
+                        type="submit"
+                        className="w-full bg-indigo-500 p-3 rounded-md text-white font-medium focus:outline-none focus:shadow-outline-indigo active:bg-indigo-600"
+                    >
+                        Create Slider
+                    </button>
+                </div>
+            </form>
+        </>
+    );
+}
+
+export default SliderForm;
