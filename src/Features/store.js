5f36667a15bd3f6171c44b98e56a7c2f3cf69ac0
tahirshaikh85 <tahirshaikhrs7@gmail.com>
2024-01-02 15:53:33 +0530

    cleanup - removed unnecessary code

--- src/Features/store.js
+++ src/Features/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import productReducer from './Product/productSlice';
 import authReducer from './Login/authSlice';
 
 export const store = configureStore({
   reducer: {
-    product: productReducer,
     auth: authReducer,
   },
 })
