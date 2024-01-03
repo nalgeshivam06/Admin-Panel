fe4c102e59e6096448be0240d481fe4b9283e53e
tahirshaikh85 <tahirshaikhrs7@gmail.com>
2024-01-03 23:25:20 +0530

    logout

--- src/components/AdminNavbar.jsx
+++ src/components/AdminNavbar.jsx
@@ -3,6 +3,10 @@ import { useNavigate } from "react-router-dom";
 
 const AdminNavbar = () => {
     const navigate = useNavigate();
+    const handleLogout = ()=>{
+        localStorage.removeItem('token');
+        navigate('/')
+    }
 
     return (
         <nav className="flex items-center justify-between md:justify-left py-4 lg:py-3  bg-black text-white px-0 md:px-6">
@@ -12,13 +16,15 @@ const AdminNavbar = () => {
 
             <div className='flex'>
                 <div className='mr-4'>
-                    <NavLink to="/create-product">Create Product</NavLink>
+                    <NavLink to="/create-product">Create Product 📦</NavLink>
                 </div>
-                <div className='mr-2'>
-                    <NavLink to="/homePage">Update Home Page</NavLink>
+                <div className='ml-4'>
+                    <NavLink to="/homePage">Update Home Page 🏠</NavLink>
                 </div>
             </div>
-
+            <div>
+                <span  className='hover:cursor-pointer' onClick={handleLogout}>Logout 🔒</span>
+            </div>
         </nav>
     );
 };
